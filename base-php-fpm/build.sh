8b8cefa32e27c6febeaccfbc13f5ec9efa202724
Alex Fernandez <7582277+alex-fernandez306@users.noreply.github.com>
2022-02-18 21:18:05 +1100

    fix: add Access Control

--- base-php-fpm/build.sh
+++ base-php-fpm/build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+<<<<<<< HEAD
 docker build -t alexfrndz/phpbase:8-fpm .
+=======
+docker buildx build --platform=linux/amd64 -t alexfrndz/phpbase:8-fpm-v1 .
+>>>>>>> f0c3caf (fix: add Access Control)

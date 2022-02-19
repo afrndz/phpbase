6f9139cfc1f6a3e6979525d31339a38a27c68687
Alex Fernandez <7582277+alex-fernandez306@users.noreply.github.com>
2022-02-19 17:01:03 +1100

    fix: add version of php fpm

--- base-php-fpm/build.sh
+++ base-php-fpm/build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
-docker build -t alexfrndz/phpbase:8-fpm .
-=======
 docker buildx build --platform=linux/amd64 -t alexfrndz/phpbase:8-fpm-v1 .
->>>>>>> f0c3caf (fix: add Access Control)
--- base-php-fpm/deploy.sh
+++ base-php-fpm/deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker push alexfrndz/phpbase:8-fpm
-
+docker push alexfrndz/phpbase:8-fpm-v1
